eddfb885dda03a87d697da7e232972bace84b3b7
Sidney Mau <sidneymau@gmail.com>
2025-11-24 10:54:44 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#/usr/bin/env bash
+
+__config_setup=$HOME/.config
+
+dotfiles=(
+	matplotlib
+	nvim
+	tmux
+	vim
+)
+
+for dotfile in "${dotfiles[@]}"; do
+	if [[ -L $__config_setup/$dotfile ]]; then
+		# echo "skipping $dotfile"
+		target=$(readlink -fv $dotfile)
+		printf "'%s' is '%s'\n" "$__config_setup/$dotfile" "$target"
+	else
+		if [[ -d $__config_setup/$dotfile && ! -L $__config_setup/$dotfile ]]; then
+			rm -irv "$__config_setup/$dotfile"
+		fi
+		ln -sv "$PWD/$dotfile" "$__config_setup/$dotfile"
+	fi
+done
